6bdc042859dcb89a81cec1682bf15e3f4331c035
AlbertTeslaWizard <albertteslawizard@gmail.com>
2026-03-28 16:54:03 +0800

    Add image generation request method

--- ReAct.py
+++ ReAct.py
@@ -4,27 +4,28 @@ from langchain_community.utilities import SerpAPIWrapper
 from langchain.tools import tool
 import os
 
-llm = ChatOpenAI(
-    model="deepseek-chat",
-    api_key=os.environ["DEEPSEEK_API_KEY"],  # 有些兼容服务会忽略它，填占位值即可
-    base_url="https://api.deepseek.com/v1",
-)
-
-search = SerpAPIWrapper()
-
 @tool
 def search_web(query: str) -> str:
     """当需要查询最新信息或模型自身不知道的知识时，使用搜索工具。"""
+    search = SerpAPIWrapper()
     return search.run(query)
 
-agent = create_agent(
-    model=llm,
-    tools=[search_web],
-    system_prompt="你是一个有帮助的研究助手。当你不确定或需要最新信息时，使用 search_web 工具。"
-)
+if __name__ == '__main__':
+    llm = ChatOpenAI(
+        model="deepseek-chat",
+        api_key=os.environ["DEEPSEEK_API_KEY"],  # 有些兼容服务会忽略它，填占位值即可
+        base_url="https://api.deepseek.com/v1",
+    )
 
-result = agent.invoke(
-    {"messages": [{"role": "user", "content": "Garnet Crow的中村由利是谁？"}]}
-)
+    agent = create_agent(
+        model=llm,
+        tools=[search_web],
+        system_prompt="你是一个有帮助的研究助手。当你不确定或需要最新信息时，使用 search_web 工具。"
+    )
 
-print(result["messages"][-1].content)
+    Query = input("请输入：")
+    result = agent.invoke(
+        {"messages": [{"role": "user", "content": Query}]}
+    )
+
+    print(result["messages"][-1].content)
